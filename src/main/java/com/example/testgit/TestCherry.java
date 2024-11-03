840407fe61d74213f3d96e1cea1a26612d0b483c
zongtailiu <321572436@qq.com>
2024-11-03 17:35:51 +0800

    new branch cherry

---
+++ src/main/java/com/example/testgit/TestCherry.java
@@ -0,0 +1,16 @@
+package com.example.testgit;
+
+/**
+ *@Author：刘宗泰
+ *@Project：TestGit
+ *@Package：com.example.testgit
+ *@Filename：TestCherry
+ *@Date：2024/11/3 17:35
+ *@要做什么：
+ */
+public class TestCherry {
+    public static void main(String[] args)
+    {
+        System.out.println("cherry");
+    }
+}
