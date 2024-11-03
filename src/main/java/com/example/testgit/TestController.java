bad8e0f1eb3e3f5029a61a1edfd273dffd523023
zongtailiu <321572436@qq.com>
2024-11-03 16:23:08 +0800

    test01

---
+++ src/main/java/com/example/testgit/TestController.java
@@ -0,0 +1,13 @@
+package com.example.testgit;
+
+/**
+ *@Author：刘宗泰
+ *@Project：TestGit
+ *@Package：com.example.testgit
+ *@Filename：TestController
+ *@Date：2024/11/3 16:21
+ *@要做什么：
+ */
+public class TestController {
+    public static final String WORD_TEST = "wordTest";
+}
