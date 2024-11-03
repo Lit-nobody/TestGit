abf7df3b85db868309dbb65402428620f788c42a
zongtailiu <321572436@qq.com>
2024-11-03 17:53:45 +0800

    Test charry test2

--- src/main/java/com/example/testgit/TestCherry.java
+++ src/main/java/com/example/testgit/TestCherry.java
@@ -12,6 +12,6 @@ public class TestCherry {
     public static void main(String[] args)
     {
         System.out.println("cherry");
-        System.out.println("cherry1");
+        System.out.println("cherry2");
     }
 }
